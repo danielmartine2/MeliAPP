7a2b758d60673dfe882d0a3f375b06c0115e124e
danielmh <dmartinez@eltiempo.com>
2022-07-18 18:56:14 -0500

    Initial Commit

---
+++ MeliApp/MeliAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeliAppApp.swift
+//  MeliApp
+//
+//  Created by Daniel Eduardo Martinez Herrera on 18/07/22.
+//
+
+import SwiftUI
+
+@main
+struct MeliAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
